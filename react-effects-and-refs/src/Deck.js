9d912e291d82fdb3ff16b8c5043426a3071eec3c
Victor Padilla <vpcorona85@gmail.com>
2023-08-13 19:39:28 -0700

    imported stylesheet to deck

--- react-effects-and-refs/src/Deck.js
+++ react-effects-and-refs/src/Deck.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState } from 'react';
 import axios from 'axios';
 import Card from './Card';
+import './Deck.css';
 
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
 
