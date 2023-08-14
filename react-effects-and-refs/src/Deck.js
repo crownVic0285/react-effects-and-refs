a09391135486df47b2d891e6da0e95e90ba687dd
Victor Padilla <vpcorona85@gmail.com>
2023-08-13 19:44:18 -0700

    modified syntax

--- react-effects-and-refs/src/Deck.js
+++ react-effects-and-refs/src/Deck.js
@@ -1,52 +1,62 @@
-import React, {useEffect, useState } from 'react';
-import axios from 'axios';
-import Card from './Card';
-import './Deck.css';
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+import axios from "axios";
+import "./Deck.css";
 
 const API_BASE_URL = "https://deckofcardsapi.com/api/deck";
+/** Deck: uses deck API, allows drawing card at a time. */
 
 function Deck() {
-    const [deck, setDeck] = useState(null);
-    const [drawn, setDrawn] = useState([]);
-    const [isShuffling, setIsShuffling] = useState(false);
+  const [deck, setDeck] = useState(null);
+  const [drawn, setDrawn] = useState([]);
+  const [isShuffling, setIsShuffling] = useState(false);
 
-    useEffect(function loadDeckFromAPI() {
-        async function getData() {
-            let d = await axios.get(`${API_BASE_URL}/new/shuffle/`);
-            setDeck(d.data);
-        }
-        getData();
-    }, [setDeck]);
+  useEffect(function loadDeckFromAPI() {
+    async function fetchData() {
+      const d = await axios.get(`${API_BASE_URL}/new/shuffle/`);
+      setDeck(d.data);
+    }
+    fetchData();
+  }, []);
 
-    async function draw() {
-        try {
-            const drawRes = await axios.get(`${API_BASE_URL}/${deck.deck_id}/draw/`);
-            if (drawRes.data.remaining === 0) throw new Error("No cards remaining!");
-            const card = drawRes.data.cards[0];
-            setDrawn(d => [
-                ...d,
-                {
-                    id: card.code,
-                    name: card.suit + " " + card.value,
-                    image: card.image,
-                },
-            ]);
+
+
+  /** Draw card: change the state & effect will kick in. */
+  async function draw() {
+    try {
+      const drawRes = await axios.get(`${API_BASE_URL}/${deck.deck_id}/draw/`);
+
+      if (drawRes.data.remaining === 0) throw new Error("Deck empty!");
+
+      const card = drawRes.data.cards[0];
+
+      setDrawn(d => [
+        ...d,
+        {
+          id: card.code,
+          name: card.suit + " " + card.value,
+          image: card.image,
+        },
+      ]);
     } catch (err) {
-        alert(err);
+      alert(err);
     }
-}
+  }
 
-    async function shuffle() {
-        setIsShuffling(true);
-        try {
-            const shuffleRes = await axios.get(`${API_BASE_URL}/${deck.deck_id}/shuffle/`);
-            setDrawn([]);
-        } catch (err) {
-            alert(err);
-        }
-        setIsShuffling(false);
+  /** Shuffle: change the state & effect will kick in. */
+  async function startShuffling() {
+    setIsShuffling(true);
+    try {
+      await axios.get(`${API_BASE_URL}/${deck.deck_id}/shuffle/`);
+      setDrawn([]);
+    } catch (err) {
+      alert(err);
+    } finally {
+      setIsShuffling(false);
     }
+  }
 
+  /** Return draw button (disabled if shuffling) */
   function renderDrawBtnIfOk() {
     if (!deck) return null;
 
@@ -60,6 +70,7 @@ function Deck() {
     );
   }
 
+  /** Return shuffle button (disabled if already is) */
   function renderShuffleBtnIfOk() {
     if (!deck) return null;
     return (
@@ -89,4 +100,3 @@ function Deck() {
 }
 
 export default Deck;
-
